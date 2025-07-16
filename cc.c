64e5e77280885372a64d745baf6c4c248c4c3318
이호준 <hjsj23430@naver.com>
2025-07-16 10:46:27 +0900

    2

--- cc.c
+++ cc.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
-int main(void){
-    printf("Hello world");
-    return 0;
+int main()
+{
+	int n; 
+	scanf("%d", &n); 
+	for (int i = 0; i < n; i++){
+		int a, b;
+		scanf("%d %d", &a, &b); 
+		int task = a;
+		for (int j = 1; j < b; j++) {
+			task = task * a %10;  
+		}
+		if (task % 10== 0) 
+			printf("%d\n", 10);
+		else  
+			printf("%d\n", task % 10);
+	}
 }
