c55ff251bf0f3f18dbc573040aa0d8789e13e9c2
이호준 <hjsj23430@naver.com>
2025-07-16 11:39:47 +0900

    jfh

--- 1037.c
+++ 1037.c
@@ -5,13 +5,13 @@ int main(){
     int arr[50];
     scanf("%d", &n);
     for (i = 0; i < n; i++){
-        scanf("%d", arr[i]);
+        scanf("%d", &arr[i]);
     }
     for (i = 0; i < n; i++){
         if (arr[i] > maxNum) maxNum = arr[i];
         if (arr[i] < minNum) minNum = arr[i];
     }
     result = maxNum * minNum;
-    printf("%d\n", result);2
+    printf("%d\n", result);
 	return 0;
 }
