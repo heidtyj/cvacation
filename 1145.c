581ce9aecee4f0aa6550e5c6a2c82b3b65d925e1
이호준 <hjsj23430@naver.com>
2025-07-16 11:33:06 +0900

    b

--- 1145.c
+++ 1145.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 int main(){
-    int arr[5] = {};
+    int n;
+    int arr[50];
     int cnt = 0, i, j;
-    for(i = 0; i < 5; i++){
+    int result;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++){
         scanf("%d", &arr[i]);
     }
-    for (i = 0; ;i++){
+    for (result = 1; ;result++){
         for (j = 0; j < 5; j++){
-            if (arr[j] % i == 0){
+            if (result % arr[j] == 0){
                 cnt++;
             }
         }
-        if (cnt >= 3){
-            printf("%d\n", i);
+        if (cnt >= n){
+            printf("%d\n", result);
             break;
         }
         else{
